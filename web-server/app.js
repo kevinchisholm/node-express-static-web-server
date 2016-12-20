899d1e3ffa3010307647e3b6064e4571c427d056
Kevin Chisholm <kevinchisholm@Kevins-MacBook-Pro.local>
2016-12-19 21:44:59 -0500

    Clean up app.js

--- web-server/app.js
+++ web-server/app.js
@@ -1,14 +1,15 @@
-var express = require('express');
-var path = require('path');
-var app = express();
+var express = require('express'),
+  path = require('path'),
+  app = express();
 
-// Define the port to run on
+//set the port
 app.set('port', 3000);
 
+//tell express that we want to use the www folder
+//for our static assets
 app.use(express.static(path.join(__dirname, '../www')));
 
 // Listen for requests
-var server = app.listen(app.get('port'), function() {
-  var port = server.address().port;
-  console.log('Magic happens on port ' + port);
+var server = app.listen(app.get('port'), function () {
+  console.log('The server is running on http://localhost:' + app.get('port'));
 });
